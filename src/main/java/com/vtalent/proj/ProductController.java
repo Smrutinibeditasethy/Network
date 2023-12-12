82baed2c6189118af84ef1abfae4e3a30fd2b7ec
SmrutiNIbeditaSethy <smrutinibeditasethy@outlook.com>
2023-12-12 16:19:18 +0530

    first commit

---
+++ src/main/java/com/vtalent/proj/ProductController.java
@@ -0,0 +1,24 @@
+package com.vtalent.proj;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
